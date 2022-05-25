0fa74e4d121f74d3b6e8faf7623c323519a92a7d
DannyLenk <93644643+DannyLenk@users.noreply.github.com>
2022-05-25 11:16:55 +0300

    built full app

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,17 @@
 import "./styles.css";
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
+import useWordsCounter from "./hooks/useWordsCounter";
 
 export default function App() {
-  const [text, setText] = useState("");
-  const [timeLeft, setTimeLeft] = useState(5);
-  const [timerStarted, setTimerStarted] = useState(false);
-  const [wordsCount, setWordsCount] = useState(0);
-  const textareaRef = useRef();
-
-  useEffect(() => {
-    if (timeLeft > 0 && timerStarted) {
-      setTimeout(() => {
-        setTimeLeft((prevState) => prevState - 1);
-      }, 1000);
-    } else if (timeLeft <= 0) {
-      endTry();
-    }
-  }, [timeLeft, timerStarted]);
-
-  function handleChange(e) {
-    setText(e.target.value);
-  }
-
-  function startTimer() {
-    setTimeLeft(5);
-    setTimerStarted(true);
-    setText("");
-    setWordsCount(0);
-    textareaRef.current.disabled = false;
-    textareaRef.current.focus();
-  }
-
-  function endTry() {
-    setTimerStarted(false);
-    setWordsCount(calculateWordsNumber(text));
-  }
-
-  function calculateWordsNumber(text) {
-    return text.split(" ").filter((char) => char !== "").length;
-  }
+  const {
+    text,
+    handleChange,
+    timerStarted,
+    textareaRef,
+    timeLeft,
+    startTimer,
+    wordsCount
+  } = useWordsCounter(10);
 
   return (
     <div className="App">
