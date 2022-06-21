6d2b2f2b63131ff912a67687a516076d7a8dc1d5
DannyLenk <93644643+DannyLenk@users.noreply.github.com>
2022-06-21 11:49:42 +0300

    implemented custom hook feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,17 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React from 'react'
+import useWordsCounter from './hooks/useWordsCounter'
 import './styles.css'
 
 function App() {
-  const [countStarted, setCountStarted] = useState(false)
-  const [timer, setTimer] = useState(5)
-  const [interval, setInterval] = useState(false)
-  const [userMsg, setUserMsg] = useState('')
-  const [wordCount, setWordCount] = useState('')
-
-  const ref = useRef(null)
-
-  useEffect(() => {
-    const timerId = setTimeout(() => {
-      if (countStarted) {
-        setTimer(prevState => prevState - 1)
-        setInterval(prevState => !prevState)  
-      }
-    }, 1000)
-    if (timer <= 0) manageTimeIsOut(timerId)
-  }, [countStarted, interval])
-
-  function startTimer() {
-    setCountStarted(true)
-    ref.current.disabled = false
-    ref.current.focus()
-    if (timer === 0) {
-      setTimer(5)
-      setWordCount(0)
-      setUserMsg('')
-    }
-  }
-
-  function manageTimeIsOut(timer) {
-    clearInterval(timer)
-    ref.current.disabled = true
-    setCountStarted(false)
-    setWordCount(userMsg.split(' ').filter(word => word !== '').length)
-  }
-
-  function handleUserInput(e) {
-    setUserMsg(e.target.value)
-  }
+  const {
+    ref,
+    userMsg,
+    startTimer,
+    handleUserInput,
+    timer,
+    countStarted,
+    wordCount                  
+  } = useWordsCounter(7)
 
   return(
     <>
--- src/hooks/useWordsCounter.jsx
+++ src/hooks/useWordsCounter.jsx
@@ -1,3 +1,55 @@
-import React from 'react'
+import { useEffect, useState, useRef } from 'react'
 
-function useWordsCounter()
+function useWordsCounter(initialTime) {
+
+   const [countStarted, setCountStarted] = useState(false)
+   const [timer, setTimer] = useState(initialTime)
+   const [userMsg, setUserMsg] = useState('')
+   const [wordCount, setWordCount] = useState('')
+ 
+   const ref = useRef(null)
+ 
+   useEffect(() => {
+     const timerId = setTimeout(() => {
+       if (countStarted) {
+         setTimer(prevState => prevState - 1)
+         setInterval(prevState => !prevState)  
+       }
+     }, 1000)
+     if (timer <= 0) manageTimeIsOut(timerId)
+   }, [countStarted, timer])
+ 
+   function startTimer() {
+     setCountStarted(true)
+     ref.current.disabled = false
+     ref.current.focus()
+     if (timer === 0) {
+       setTimer(initialTime)
+       setWordCount(0)
+       setUserMsg('')
+     }
+   }
+ 
+   function manageTimeIsOut(timer) {
+     clearInterval(timer)
+     ref.current.disabled = true
+     setCountStarted(false)
+     setWordCount(userMsg.split(' ').filter(word => word !== '').length)
+   }
+ 
+   function handleUserInput(e) {
+     setUserMsg(e.target.value)
+   } 
+   
+   return({
+      ref,
+      userMsg,
+      startTimer,
+      handleUserInput,
+      timer,
+      countStarted,
+      wordCount                  
+   })
+}
+
+export default useWordsCounter
